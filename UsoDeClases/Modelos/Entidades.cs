3c223fa70e7e11b361d9de0bc85e1d39e2adefd8
IngFranciscoMena <franciscodaniel.penate@unab.edu.sv>
2025-02-25 20:34:08 -0600

    Se suben cambios de Declaración y uso de clases.

---
+++ UsoDeClases/Modelos/Entidades.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsoDeClases.Modelos
+{
+    public class Persona
+    {
+        // Propiedades de la clase Persona
+        public string nombres; 
+        public string apellidos;
+        public int edad;
+        public DateTime fechaNacimiento; 
+        public string documento; 
+        public string telefono; 
+        public string direccion; 
+        public string email;
+
+        // Constructor
+        public Persona(string nombres, string apellidos, int edad, DateTime fechaNacimiento, string documento, string telefono
+            , string direccion, string email)
+        {
+                    this.nombres = nombres;
+                    this.apellidos = apellidos;
+                    this.edad = edad;
+                    this.fechaNacimiento = fechaNacimiento;
+                    this.documento = documento;
+                    this.telefono = telefono;
+                    this.direccion = direccion;
+                    this.email = email;
+        }
+
+        // Métodos
+        public string MostrarDatos()
+        {
+            return $"Nombre: {this.nombres}\nApellidos: {this.apellidos}\nEdad: {this.edad}\nEmail: {this.email}\nTelefono: {this.telefono}\nDireccion: {this.direccion}\n";
+        }
+    }
+
+
+}
---
+++ UsoDeClases/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UsoDeClases.Modelos;
+
+namespace UsoDeClases
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instancar la clase Persona, en otras palabras crear un objeto
+            Persona persona1 = new Persona("Mario", "Alvarenga", 18, DateTime.Parse("2007/05/02"), "06223410-5", "7023-0000", "San Salvador Centro", "marioalvarenga@example.com"); // Creamos el objeto "persona1"
+            // Mostrar datos del objeto persona1
+            Console.WriteLine(persona1.MostrarDatos());
+
+            // Crear objeto persona2
+            Persona persona2 = new Persona("Julio", "Menjivar", 19, DateTime.Parse("2006/08/02"), "06223850-5", "7000-4000", "San Salvador Norte", "juliomenjivar@example.com");
+            // Mostrar datos del objeto persona2
+            Console.WriteLine(persona2.MostrarDatos());
+
+            Console.ReadLine();
+        }
+    }
+}
